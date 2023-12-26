14e895c250278dc41dd63e51d3907df16aff0b0a
Zzazzfet <112476145+zzfet@users.noreply.github.com>
2023-12-26 21:36:23 +0000

    Update renamerOnUpdate.py
    Very loose patching for v24 - Pretty much just removed the oshash from the GraphQL calls and swapped 'rating' for 'rating100'. Very much intended as a 'day one' fix to get people back up and running after switching to v24.

--- plugins/renamerOnUpdate/renamerOnUpdate.py
+++ plugins/renamerOnUpdate/renamerOnUpdate.py
@@ -106,11 +106,9 @@ def graphql_getScene(scene_id):
     }
     fragment SceneData on Scene {
         id
-        oshash
-        checksum
         title
         date
-        rating
+        rating100
         stash_ids {
             endpoint
             stash_id
@@ -133,7 +131,7 @@ def graphql_getScene(scene_id):
             name
             gender
             favorite
-            rating
+            rating100
             stash_ids{
                 endpoint
                 stash_id
@@ -168,11 +166,9 @@ def graphql_findScene(perPage, direc="DESC") -> dict:
     }
     fragment SlimSceneData on Scene {
         id
-        oshash
-        checksum
         title
         date
-        rating
+        rating100
         organized
         stash_ids {
             endpoint
@@ -196,7 +192,7 @@ def graphql_findScene(perPage, direc="DESC") -> dict:
             name
             gender
             favorite
-            rating
+            rating100
             stash_ids{
                 endpoint
                 stash_id
@@ -467,7 +463,6 @@ def extract_info(scene: dict, template: None):
     # note: basename contains the extension
     scene_information['current_filename'] = os.path.basename(scene_information['current_path'])
     scene_information['current_directory'] = os.path.dirname(scene_information['current_path'])
-    scene_information['oshash'] = scene['oshash']
     scene_information['checksum'] = scene.get("checksum")
     scene_information['studio_code'] = scene.get("code")
 
@@ -506,8 +501,8 @@ def extract_info(scene: dict, template: None):
         scene_information['duration'] = str(scene_information['duration'])
 
     # Grab Rating
-    if scene.get("rating"):
-        scene_information['rating'] = RATING_FORMAT.format(scene['rating'])
+    if scene.get("rating100"):
+        scene_information['rating100'] = RATING_FORMAT.format(scene['rating100'])
 
     # Grab Performer
     scene_information['performer_path'] = None
@@ -984,7 +979,7 @@ def file_rename(current_path: str, new_path: str, scene_info: dict):
         if LOGFILE:
             try:
                 with open(LOGFILE, 'a', encoding='utf-8') as f:
-                    f.write(f"{scene_info['scene_id']}|{current_path}|{new_path}|{scene_info['oshash']}\n")
+                    f.write(f"{scene_info['scene_id']}|{current_path}|{new_path}\n")
             except Exception as err:
                 shutil.move(new_path, current_path)
                 log.LogError(f"Restoring the original path, error writing the logfile: {err}")
@@ -1045,11 +1040,6 @@ def renamer(scene_id, db_conn=None):
                 "type": "md5",
                 "value": stash_scene["checksum"]
             })
-        if stash_scene.get("oshash"):
-            fingerprint.append({
-                "type": "oshash",
-                "value": stash_scene["oshash"]
-            })
         stash_scene["file"]["fingerprints"] = fingerprint
         scene_files = [stash_scene["file"]]
         del stash_scene["path"]
@@ -1064,8 +1054,6 @@ def renamer(scene_id, db_conn=None):
         scene_file = scene_files[i]
         # refractor file support
         for f in scene_file["fingerprints"]:
-            if f.get("oshash"):
-                stash_scene["oshash"] = f["oshash"]
             if f.get("md5"):
                 stash_scene["checksum"] = f["md5"]
         stash_scene["path"] = scene_file["path"]
@@ -1158,14 +1146,6 @@ def renamer(scene_id, db_conn=None):
             continue
         # check if there is already a file where the new path is
         err = checking_duplicate_db(scene_information)
-        while err and scene_information['file_index']<=len(DUPLICATE_SUFFIX):
-            log.LogDebug("Duplicate filename detected, increasing file index")
-            scene_information['file_index'] = scene_information['file_index'] + 1
-            scene_information['new_filename'] = create_new_filename(scene_information, template["filename"])
-            scene_information['final_path'] = os.path.join(scene_information['new_directory'], scene_information['new_filename'])
-            log.LogDebug(f"[NEW filename] {scene_information['new_filename']}")
-            log.LogDebug(f"[NEW path] {scene_information['final_path']}")
-            err = checking_duplicate_db(scene_information)
         # abort
         if err:
             raise Exception("duplicate")
@@ -1367,4 +1347,3 @@ else:
         traceback.print_exc()
 
 exit_plugin("Successful!")
-
